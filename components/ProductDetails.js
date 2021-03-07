7e33982697c362d71c503f35f8c2f8601fb2a28f
rina.miyata <rina.miyata@eilsystem.com>
2021-03-07 19:25:35 +0900

    データをdetailsに持ってくる

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -10,9 +10,9 @@ app.component('product-details', {
     `<ul>
     <li v-for="detail in details">{{ detail }}</li>
   </ul>`,
-  // data() {
-  //   return {
-  //     details: ['50% cotton', '30% wool', '20% polyester'],
-  //   }
-  // }
+  data() {
+    return {
+      details: ['50% cotton', '30% wool', '20% polyester'],
+    }
+  }
 })
