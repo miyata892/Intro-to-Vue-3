247c6a145fab667980d509540e535cdb920fc89f
rina.miyata <rina.miyata@eilsystem.com>
2021-03-07 17:58:32 +0900

    他の属性を試す

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
           product: 'Socks',
-          image: './assets/images/socks_blue.jpg'
+          image: './assets/images/socks_blue.jpg',
+          description: 'Text',
+          url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3'
         }
     }
 })
