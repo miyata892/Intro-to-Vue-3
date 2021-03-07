f1a535452bd06642b667b58988c7ca02cc56a63f
rina.miyata <rina.miyata@eilsystem.com>
2021-03-07 18:46:34 +0900

    intro to vue L8

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const app = Vue.createApp({
     data() {
-        return {
+    return {
+          onSale: true,
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
+            // inStock: true,
+          details: ['50% cotton', '30% wool', '20% polyester'],
+            selectVariant: 0,
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
     },
@@ -17,8 +19,20 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+          this.selectVariant = index;
+          // console.log(index);
         }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product + ' ' + (this.onSale ? 'is on sale ' : '');
+    },
+    image() {
+      return this.variants[this.selectVariant].image
+    },
+    inStock() {
+      return this.variants[this.selectVariant].quantity
     }
+  },
 })
